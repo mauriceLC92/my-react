8fa7e4caf3be01bf4f6c7c862517440d6613c119
Maurice Le Cordier <maurice.lecordier@SAMAC141.local>
2018-02-20 11:04:27 +0200

    changed HorseCardListEmpty to a functional component

--- src/components/HorseCard/HorseCardListEmpty.js
+++ src/components/HorseCard/HorseCardListEmpty.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
-export class HorseCardListEmpty extends React.Component{
-    render(){
-        return(
-            <div className="ui icon message">
-                <i className="notched circle loading icon"></i>
-                <div className="content">
-                    <div className="header">
-                        Waiting to generate Horses
-                    </div>
+const HorseCardListEmpty = (props) =>{
+    return(
+        <div className="ui icon message">
+            <i className="notched circle loading icon"></i>
+            <div className="content">
+                <div className="header">
+                    Waiting to generate Horses
                 </div>
             </div>
-            
-        );
-    }
+        </div>
+        
+    );
 }
+
+export default HorseCardListEmpty;
